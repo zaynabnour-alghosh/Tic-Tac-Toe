45ea086959e3145b6ca4a4135a6e8886c2ac02f7
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-13 21:10:38 +0300

    getting references of the dom objects

---
+++ script.js
@@ -0,0 +1,15 @@
+let player1 = document.getElementById('player1')
+let player2 = document.getElementById('player2')
+let s1=document.getElementById('score-p1')
+let s2=document.getElementById('score-p2')
+let btn_Go=document.getElementById('go')
+let btn_PlayAgain=document.getElementById('playAgain')
+let boxes = Array.from(document.getElementsByClassName('box'))
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+let arr=Array(9).fill("")
+const O="O"
+const X="X"
+let currentPlayer=X
+let winner=''
+playerT=document.getElementById('playerText')
