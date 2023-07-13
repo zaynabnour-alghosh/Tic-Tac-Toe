f700c5b5204aab389e4969a1ce944fb509ba6145
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-13 21:14:54 +0300

    adding the winning combinations

--- script.js
+++ script.js
@@ -59,3 +59,13 @@ function boxClicked(e){
     s1.innerHTML=scorePlayer1
     s2.innerHTML=scorePlayer2    
 }
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
