6b3a25f61e602568c258cb7b9e3575ed64c11f37
ZeinabNour-Gh02 <106086653+ZeinabNour-Gh02@users.noreply.github.com>
2023-07-13 21:13:03 +0300

    adding event listener for Go button

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const X="X"
 let currentPlayer=X
 let winner=''
 playerT=document.getElementById('playerText')
+btn_Go.addEventListener('click', go)
+// console.log(arr)
+function go() {
+    btn_Go.style.display='none'
+    span_p1=document.getElementById('P1-disabled')
+    span_p2=document.getElementById('P2-disabled')
+    span_p1.textContent=player1.value
+    span_p2.textContent=player2.value
+    span_p1.style.display="block"
+    span_p2.style.display="block"
+    for(let i=0; i<boxes.length;i++){
+        boxes[i].addEventListener('click',boxClicked)
+    }
+//     console.log(boxes)
+//     console.log(player1.value)
+//     console.log(player2.value)
+// }
+}
